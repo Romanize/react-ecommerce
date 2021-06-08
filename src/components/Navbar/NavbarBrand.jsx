1d0cd2580baddf79ec23d043dc475328486ad40b
Robert <robert.g.martinez.a@gmail.com>
2021-06-08 00:53:16 -0300

    Typologo

--- src/components/Navbar/NavbarBrand.jsx
+++ src/components/Navbar/NavbarBrand.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './NavbarBrand.css';
-import logo from './NavbarBrand--Logo.png'
+import { Typography } from '@material-ui/core';
+
+import logo from './BrandLogo.png'
 
 const NavbarBrand = () => {
-    const brand__name = 'SOLODEPORTES';
+    const brand__name = 'Brand Name';
 
     return ( 
         <div className="NavbarBrand">
             <a href="/index">
                 <img src={logo} alt="Brand Logo" />
-                {brand__name}
+                <Typography variant="h6" color="inherit">
+                    {brand__name}
+                </Typography>
             </a>
         </div>
      );
